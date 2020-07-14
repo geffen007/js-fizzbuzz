fe3fca27a50211a751a971430fe6bfe29c827133
Guido Amodio <guido.amodio@gmail.com>
2020-07-14 15:53:00 +0200

    spazi, varie ed eventuali

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ for (var i = 1; i <= 100; i++) {
 
     // creo una variabile uguale a i da andare a sovrascrivere in modo che i va fino a 100 senza problemi e "numero" possa essere sovrascritta senza problemi
     numero=i;
-    if ((i%3 ==0)&&(i%5==0)){
+    if ((i%3 == 0)&&(i%5 == 0)){
         numero="fizz"+"buzz";    //prima la consizione più complessa
     } else if (i%5 == 0) {
         numero= "buzz";           // e poi il resto
